67ac43156628d702fb8eb269a1dc598fb8cfd039
test <declanpj05@gmail.com>
2026-01-23 19:29:10 -0500

    Added animation toggle

--- assets/js/float.js
+++ assets/js/float.js
@@ -1,10 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('anim-toggle');
     if(localStorage.getItem("animated")){
         animated = localStorage.getItem("animated")
     } else {
-        animated = true;
+        animated = "true";
         localStorage.setItem("animated", animated);
     }
+    if(animated == "true"){
+        toggleButton.textContent = ">";
+    }
+    else{
+        toggleButton.textContent = "<";
+    }
+        toggleButton.addEventListener('click', () => {
+        if(animated == "true"){
+            animated = "false";
+            floating();
+            toggleButton.textContent = "<";
+            localStorage.setItem("animated", animated);
+        }
+        else{
+            animated = "true";
+            floating();
+            toggleButton.textContent = ">";
+            localStorage.setItem("animated", animated);
+        }
+        });
     floating();
 });
 
@@ -22,12 +43,17 @@ const values = [...elements].map(el => {
 });
 
 function floating(time) {
-    if(!animated) return;
 
     for (const box of values) {
-        const offsetX = Math.sin(time * box.freqX + box.phaseX) * 5;
-        const offsetY = Math.cos(time * box.freqY + box.phaseY) * 5;
-        box.el.style.transform = `translateX(${offsetX}px) translateY(${offsetY}px)`
+        if(localStorage.getItem("animated") == "false"){
+            box.el.style = 'transition-duration: 0ms';
+            box.el.style.transform = 'none';
+        }
+        else{
+            const offsetX = Math.sin(time * box.freqX + box.phaseX) * 3;
+            const offsetY = Math.cos(time * box.freqY + box.phaseY) * 3;
+            box.el.style.transform = `translateX(${offsetX}px) translateY(${offsetY}px)`;
+        }
     };
     requestAnimationFrame(floating);
 
